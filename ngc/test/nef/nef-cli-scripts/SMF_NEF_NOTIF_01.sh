6bb57915872f8b7f13311622043959723564b651
Sudhir Pola <sudhir.pola@intel.com>
2019-11-27 07:30:51 +0530

    aling HTTP1.1 config and scripts to port 8091

--- ngc/test/nef/nef-cli-scripts/SMF_NEF_NOTIF_01.sh
+++ ngc/test/nef/nef-cli-scripts/SMF_NEF_NOTIF_01.sh
@@ -4,9 +4,7 @@ setup_dir=${PWD}
 
 set -e
 
-# curl -X POST -i "Content-Type: application/json" --data @./json/SMF_NEF_POST_01.json http://localhost:8091/3gpp-traffic-influence/v1/AF_01/subscriptions
-
-curl -X POST -i "Content-Type: application/json" --data @./json/SMF_NEF_NOTIF_01.json http://localhost:8080/3gpp-traffic-influence/v1/notification/upf
+curl -X POST -i "Content-Type: application/json" --data @./json/SMF_NEF_NOTIF_01.json http://localhost:8091/3gpp-traffic-influence/v1/notification/upf
 
 exit 0
 
