bb24083aad923333f81811e2cdcb5d314c487f92
Sudhir Pola <sudhir.pola@intel.com>
2019-11-29 09:06:59 +0530

    Comment fix

--- ngc/pkg/nef/nef_udr_id_client.go
+++ ngc/pkg/nef/nef_udr_id_client.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* Client implementation of the pcf stub */
+/* Client implementation of the UDR Stub */
 
 package ngcnef
 
