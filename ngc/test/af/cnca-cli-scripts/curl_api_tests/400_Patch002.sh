24bfc623aa233fb77a0c72c876714a0d2afd6045
nidhibelur <nidhi.shivashankara.belur@intel.com>
2019-12-05 18:05:08 -0800

    [OP-1395] cnca stub 	- addressed review comments
    Signed-off-by: nidhibelur <nidhi.shivashankara.belur@intel.com>

--- ngc/test/af/cnca-cli-scripts/curl_api_tests/400_Patch002.sh
+++ ngc/test/af/cnca-cli-scripts/curl_api_tests/400_Patch002.sh
@@ -1,5 +1,5 @@
-###SPDX-License-Identifier: Apache-2.0
-###Copyright © 2019 Intel Corporation
+#SPDX-License-Identifier: Apache-2.0
+#Copyright © 2019 Intel Corporation
 
 #! /bin/sh
 setup_dir=${PWD}
