48b015d17862e62730e2c8f9f2e6a9c5c428b14c
SONIA BHATTACHARYA <sonia.bhattacharya@intel.com>
2020-01-23 10:06:46 +0530

    Minor change reverted in nef_hdlr.go

--- ngc/pkg/nef/nef_hdlr.go
+++ ngc/pkg/nef/nef_hdlr.go
@@ -73,7 +73,7 @@ type nefSBRspData struct {
 	pd        ProblemDetails
 }
 
-//Creates an AF instance
+//Creates a AF instance
 func (af *afData) afCreate(nefCtx *nefContext, afID string) error {
 
 	//Validate afid ??
