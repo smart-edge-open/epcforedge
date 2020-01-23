9a0150f3f2e9e9232da27ce42265d1470cc50f8e
SONIA BHATTACHARYA <sonia.bhattacharya@intel.com>
2020-01-23 12:04:42 +0530

    PFD model for UDR

--- ngc/pkg/nef/model_nef_af.go
+++ ngc/pkg/nef/model_nef_af.go
@@ -143,19 +143,20 @@ type TemporalValidity struct {
 	StopTime string `json:"stopTime,omitempty"`
 }
 
-// Pfd is the structure of PFD for an external Application Identifier
+// Pfd is the structure of Packet Flow Description for an external Application
+// Identifier
 type Pfd struct {
 	// Identifies a PDF of an application identifier.
 	PfdId string `json:"pfdId"`
 	// Represents a 3-tuple with protocol, server ip and server port for UL/DL
-	//application traffic. The content of the string has the same encoding as
-	//the IPFilterRule AVP value as defined in IETFÂ RFCÂ 6733.
+	// application traffic. The content of the string has the same encoding as
+	// the IPFilterRule AVP value as defined in IETFÂ RFCÂ 6733.
 	FlowDescriptions []string `json:"flowDescriptions,omitempty"`
 	// Indicates a URL or a regular expression which is used to match the
-	//significant parts of the URL.
+	// significant parts of the URL.
 	Urls []string `json:"urls,omitempty"`
 	// Indicates an FQDN or a regular expression as a domain name matching
-	//criteria.
+	// criteria.
 	DomainNames []string `json:"domainNames,omitempty"`
 }
 
@@ -164,40 +165,40 @@ type Pfd struct {
 type PfdData struct {
 	// Each element uniquely identifies external application identifier
 	ExternalAppId string `json:"externalAppId"`
-	//Link to the resource. This parameter shall be supplied by the NEF in
-	//HTTP responses that include an object of PfdData type
+	// Link to the resource. This parameter shall be supplied by the NEF in
+	// HTTP responses that include an object of PfdData type
 	Self Link `json:"self,omitempty"`
 	// Contains the PFDs of the external application identifier. Each PFD is
-	//identified in the map via a key containing the PFD identifier.
+	// identified in the map via a key containing the PFD identifier.
 	Pfds map[string]Pfd `json:"pfds"`
-	//Indicates that the list of PFDs in this request should be deployed within
-	//the time interval indicated by the Allowed Delay
+	// Indicates that the list of PFDs in this request should be deployed within
+	// the time interval indicated by the Allowed Delay
 	AllowedDelay DurationSecRm `json:"allowedDelay,omitempty"`
-	//SCEF supplied property, inclusion of this property means the allowed
-	//delayed cannot be satisfied, i.e. it is smaller than the caching time,
-	//but the PFD data is still stored.
+	// SCEF supplied property, inclusion of this property means the allowed
+	// delayed cannot be satisfied, i.e. it is smaller than the caching time,
+	// but the PFD data is still stored.
 	CachingTime DurationSecRo `json:"cachingTime,omitempty"`
 }
 
 // PfdManagement resource for a PFD management request
 type PfdManagement struct {
-	//Link to the resource "Individual PFD Management Transaction".
-	//This parameter shall be supplied by the NEF in HTTP responses.
+	// Link to the resource "Individual PFD Management Transaction".
+	// This parameter shall be supplied by the NEF in HTTP responses.
 	Self Link `json:"self,omitempty"`
-	//String identifying supported optional features of PFD Management
-	//This attribute shall be provided in the POST request and in the
-	//response of successful resource creation.
-	SuppFeat SupportedFeatures `json:"supportedFeatures,omitempty"`
+	// String identifying supported optional features of PFD Management
+	// This attribute shall be provided in the POST request and in the
+	// response of successful resource creation.
+	SuppFeat SupportedFeatures `json:"suppFeat,omitempty"`
 	// Each element uniquely identifies the PFDs for an external application
-	//identifier. Each element is identified in the map via an external
-	//application identifier as key. The response shall include successfully
-	//provisioned PFD data of application(s).
+	// identifier. Each element is identified in the map via an external
+	// application identifier as key. The response shall include successfully
+	// provisioned PFD data of application(s).
 	PfdDatas map[string]PfdData `json:"pfdDatas"`
 	// Supplied by the AF and contains the external application identifiers
-	//for which PFD(s) are not added or modified successfully. The failure
-	//reason is also included. Each element provides the related information for
+	// for which PFD(s) are not added or modified successfully. The failure
+	// reason is also included. Each element provides the related information for
 	// one or more external application identifier(s) and is identified in the
-	//map via the failure identifier as key.
+	// map via the failure identifier as key.
 	PfdReports map[string]PfdReport `json:"pfdReports,omitempty"`
 }
 
@@ -206,17 +207,17 @@ type FailureCode string
 
 // Possible values of FailureCode
 const (
-	//This value indicates that something functions wrongly in PFD provisioning
-	//or the PFD provisioning does not function at all.
+	// This value indicates that something functions wrongly in PFD provisioning
+	// or the PFD provisioning does not function at all.
 	MALFUNCTION FailureCode = "MALFUNCTION"
 	// This value indicates there is resource limitation for PFD storage.
 	RESOURCE_LIMITATION = "RESOURCE_LIMITATION"
-	//This value indicates that the allowed delay is too short and PFD(s) are
-	//not stored
+	// This value indicates that the allowed delay is too short and PFD(s) are
+	// not stored
 	SHORT_DELAY = "SHORT_DELAY"
-	//The received external application identifier(s) are already provisioned
+	// The received external application identifier(s) are already provisioned
 	APP_ID_DUPLICATED = "APP_ID_DUPLICATED"
-	//Other reason specified
+	// Other reason specified
 	OTHER_REASON = "OTHER_REASON"
 )
 
@@ -224,12 +225,12 @@ const (
 // external application identifier(s) which PFD(s) are not added or
 // modified successfully and corresponding failure reason.
 type PfdReport struct {
-	//Identifies the external application identifier(s) which PFD(s) are not
-	//added or modified successfully
+	// Identifies the external application identifier(s) which PFD(s) are not
+	// added or modified successfully
 	ExternalAppIds []string `json:"externalAppIds"`
-	//Identifies the failure reason
+	// Identifies the failure reason
 	FailureCode FailureCode `json:"failureCode"`
-	//It shall be included when the allowed delayed cannot be satisfied, i.e.
-	//it is smaller than the caching time configured in fetching PFD.
+	// It shall be included when the allowed delayed cannot be satisfied, i.e.
+	// it is smaller than the caching time configured in fetching PFD.
 	CachingTime DurationSec `json:"cachingTime,omitempty"`
 }
