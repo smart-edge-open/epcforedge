5db39230bcc2400cab0e382718cb1fea3a971791
SONIA BHATTACHARYA <sonia.bhattacharya@intel.com>
2020-02-04 11:06:50 +0530

    Fixing the Delete PFD management in AF

--- ngc/pkg/af/pfd_management_transaction_delete.go
+++ ngc/pkg/af/pfd_management_transaction_delete.go
@@ -6,17 +6,16 @@ package af
 import (
 	"context"
 	"net/http"
-	"strconv"
 )
 
-func deleteSubscription(cliCtx context.Context, afCtx *Context,
-	sID string) (*http.Response, error) {
+func deletePfdTransaction(cliCtx context.Context, afCtx *Context,
+	pfdID string) (*http.Response, error) {
 
 	cliCfg := NewConfiguration(afCtx)
 	cli := NewClient(cliCfg)
 
-	resp, err := cli.TrafficInfluSubDeleteAPI.SubscriptionDelete(cliCtx,
-		afCtx.cfg.AfID, sID)
+	resp, err := cli.PfdManagementDeleteAPI.PfdTransactionDelete(cliCtx,
+		afCtx.cfg.AfID, pfdID)
 
 	if err != nil {
 		return resp, err
@@ -25,30 +24,18 @@ func deleteSubscription(cliCtx context.Context, afCtx *Context,
 
 }
 
-// DeleteSubscription function
-func DeleteSubscription(w http.ResponseWriter, r *http.Request) {
+// DeletePfdTransaction function
+func DeletePfdTransaction(w http.ResponseWriter, r *http.Request) {
 	var (
-		err            error
-		resp           *http.Response
-		subscriptionID string
+		err      error
+		resp     *http.Response
+		pfdTrans string
 	)
 
 	afCtx := r.Context().Value(keyType("af-ctx")).(*Context)
 	if afCtx == nil {
-		log.Errf("Traffic Influance Subscription delete: " +
+		log.Errf("Pfd Transaction delete: " +
 			"af-ctx retrieved from request is nil")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if afCtx.subscriptions == nil {
-		log.Errf("AF context subscriptions map has not been initialized")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if afCtx.transactions == nil {
-		log.Errf("AF context  transactions map has not been initialized")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -58,30 +45,17 @@ func DeleteSubscription(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-	subscriptionID, err = getSubsIDFromURL(r.URL)
+	pfdTrans, err = getPfdTransIDFromURL(r.URL)
 	if err != nil {
-		log.Errf("Traffic Influence Subscription delete: %s", err.Error())
+		log.Errf("Pfd Transaction delete %s", err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	resp, err = deleteSubscription(cliCtx, afCtx, subscriptionID)
+	resp, err = deletePfdTransaction(cliCtx, afCtx, pfdTrans)
 	if err != nil {
-		log.Errf("Traffic Influence Subscription delete: %s", err.Error())
+		log.Errf("Pfd Transaction delete %s", err.Error())
 		w.WriteHeader(getStatusCode(resp))
 		return
-	}
-
-	if interMap, ok := afCtx.subscriptions[subscriptionID]; ok {
-		for transID := range interMap {
-			var i int
-			if i, err = strconv.Atoi(transID); err != nil {
-				log.Errf("Error converting transID to integer: %v", err)
-			} else {
-				delete(afCtx.transactions, i)
-				log.Infof("Deleted transaction ID %v", i)
-			}
-		}
-		delete(afCtx.subscriptions, subscriptionID)
 	}
 
 	w.WriteHeader(resp.StatusCode)
