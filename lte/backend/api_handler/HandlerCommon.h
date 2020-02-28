e37c55e3efcaf9e155c97a250152e6064482c8d8
Sushil Lakra <sushil.lakra@intel.com>
2020-02-28 14:53:07 +0530

    changes for 4G-OAM File Headers, ERROR:GLOBAL_INITIALISERS: warnings in 4G-OAM and 5G-OAM Lint Errors

--- lte/backend/api_handler/HandlerCommon.h
+++ lte/backend/api_handler/HandlerCommon.h
@@ -1,18 +1,7 @@
-/*******************************************************************************
-* Copyright 2019 Intel Corporation. All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2019 Intel Corporation
+ */
+
 /**
  * @file    HandlerCommon.h
  * @brief   Header file for common declarations.
@@ -42,9 +31,9 @@ const char RULE_FIELD_VALUE_EQUAL_MARK = '=';
 const char RULE_FIELD_MIN_MIN_SPLIT_MARK = '-';
 const char RULE_FIELD_IP_MASK_SPLIT_MARK = '/';
 
-const uint32_t IP_MASK_MIN = 0;
+const uint32_t IP_MASK_MIN;
 const uint32_t IP_MASK_MAX = 32;
-const uint32_t PORT_MIN = 0;
+const uint32_t PORT_MIN;
 const uint32_t PORT_MAX = 65535;
 
 const uint32_t SERVICE_ID_LENGTH = 64;
