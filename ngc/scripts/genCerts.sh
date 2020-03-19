f8d80ac4346dfbce47cb9f44f757f91549436013
SONIA BHATTACHARYA <sonia.bhattacharya@intel.com>
2020-03-19 12:34:39 +0530

    Revert "Fix for provision of providing  IP addresses as well in certificate generation"
    This reverts commit 03114ee13701709a9455eaaf0957abb70d4233b1.

--- ngc/scripts/genCerts.sh
+++ ngc/scripts/genCerts.sh
@@ -6,90 +6,47 @@
 helpPrint()
 {
    echo ""
-   echo "Usage: $0 [-t sanType -h {sanValue | *}] | *"
+   echo "Usage: $0 -t sanType -h {subj | *}"
    echo -e "\t-t SAN type: could be IP or DNS"
    echo -e "\t-h subject alternative names list: could be one or more IP addressess or domain names separated by space"
-   echo -e "\t"
-   echo -e "Ex: $0 -t DNS -h afservice nefservice localhost"
-   echo -e "    $0 -t IP -h 172.168.10.12 172.168.10.56"
-   echo -e "    $0 -t DNS -h controller -t IP -h 172.168.10.12 172.168.10.56"
-   echo -e "    $0 -t IP -h 172.168.10.12 -t DNS -h afservice nefservice localhost"
-   echo -e ""
    exit 1 # Exit with help
 }
 
 Hostname_count=0
 Hostname_flag=0
-IP_count=0
-IP_flag=0
 subjstr=""
-subj1=""
-tupleStarted=0
 
 while [ "$1" != "" ]; 
 do
    case $1 in
       -t )
-         if [ $tupleStarted == 1 ]
-         then
-            echo "Missing -h option"
-            helpPrint
-         else
-            tupleStarted=1
-         fi
          if [ $Hostname_flag == 1 ]
          then
-            Hostname_flag=0
-         fi
-         if [ $IP_flag == 1 ]
-         then
-            IP_flag=0
+            echo "Incorrect Command Sequence"
+            helpPrint
          fi
          shift 
-         if [ "$1" == IP ] || [ "$1" == DNS ] 
+         if [ "$1" != "" ]
          then
             sanType="$1"
          else
-            echo "Missing/Wrong sanType"
+            echo "Missing argument for -t option"
             helpPrint
          fi
          ;;
       -h )
-         if [ "$sanType" == "" ] || [ $Hostname_flag != 0 ] || [ $IP_flag != 0 ]
+         if [ "$sanType" == "" ]
          then
-            echo "Missing -t option"
+            echo "Incorrect Command Sequence. Got -h option before -t option"
             helpPrint
          fi
          shift
-         if [ "$1" != "" ] && [ "$1" != "-t" ] && [ "$1" != "-h" ]
+         if [ "$1" != "" ]
          then
-            tupleStarted=0
-            if [ "$subj1" == "" ]
-            then
-               subj1="$1"
-            fi
-            if [ "$sanType" == DNS ]
-            then
-               Hostname_flag=$((Hostname_flag+1))
-               Hostname_count=$((Hostname_count+1))
-               if [ "$subjstr" == "" ]
-               then
-                  subjstr=$sanType"."$Hostname_count":""$1"
-               else
-                  subjstr+=","$sanType"."$Hostname_count":""$1"
-               fi
-            fi
-            if [ "$sanType" == IP ]
-            then
-               IP_flag=$((IP_flag+1))
-               IP_count=$((IP_count+1))
-               if [ "$subjstr" == "" ]
-               then 
-                  subjstr=$sanType"."$IP_count":""$1"
-               else
-                  subjstr+=","$sanType"."$IP_count":""$1"
-               fi
-            fi
+            subj1="$1"
+            Hostname_flag=$((Hostname_flag+1))
+            Hostname_count=$((Hostname_count+1))
+            subjstr=$sanType"."$Hostname_count":""$1"
          else
             echo "Missing argument for -h option"
             helpPrint
@@ -102,10 +59,6 @@ do
          then
             Hostname_count=$((Hostname_count+1))
             subjstr+=","$sanType"."$Hostname_count":""$1"
-         elif [ $IP_flag == 1 ]
-         then
-            IP_count=$((IP_count+1))
-            subjstr+=","$sanType"."$IP_count":""$1"
          else
             echo "Incorrect Input"
             helpPrint
@@ -115,19 +68,24 @@ do
   shift
 done
 
-if [ $tupleStarted != 0 ]
-then
-   echo "Missing -h option "
-   helpPrint
-fi
-
 if [ -z "$subj1" ] || [ -z "$subjstr" ] || [ -z "$sanType" ]
 then
    echo "One of the input parameters missing"
    helpPrint
 fi
 
+if [ "$sanType" == IP ] || [ "$sanType" == DNS ]
+then 
+   echo "Input OK"
+else
+   echo "Wrong sanType"
+   helpPrint
+   exit 1
+fi
+
 echo "Running with input parameters:"
+echo "$sanType"
+#echo "$subj1"
 echo "$subjstr"
 
 ROOT_CA_NAME=OpenNESS-5G-Root
@@ -162,6 +120,7 @@ then
    exit 1
 fi
 rm -f extfile.cnf
+#echo "subjectAltName = $sanType.1:$subj1,$sanType.2:$subj2,$sanType.3:$subj3,$sanType.4:$subj4" >> extfile.cnf
 echo "subjectAltName = $subjstr" >> extfile.cnf
 openssl x509 -req -extfile extfile.cnf -in "server-request.csr" -CA "root-ca-cert.pem" -CAkey "root-ca-key.pem" -days 90 -out "server-cert.pem" -CAcreateserial
 if (($?))
