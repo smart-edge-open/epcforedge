790fb2301627390aa953ff98b007ce5bc6ce0bf4
Niket Kumar <niket.kumar@intel.com>
2020-04-15 16:20:16 +0530

    Introduced new nicroservice cn-test under ngc-test

---
+++ ngc/pkg/cn-test/cn_test_router.go
@@ -0,0 +1,162 @@
+/* SPDX-License-Identifier: Apache-2.0
+* Copyright (c) 2019-2020 Intel Corporation
+ */
+
+ package ngccntest
+
+ import (
+	"context"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
+	oauth2 "github.com/otcshare/epcforedge/ngc/pkg/oauth2"
+)
+
+type cnTestCtxKey string
+
+// Route : Structure which describes HTTP Request Handler type and other
+//         details like name, method, path, etc
+type Route struct {
+	Name    string
+	Method  string
+	Pattern string
+	Handler http.HandlerFunc /*    */
+}
+
+// NEFRoutes : NEF Routes lists which contains Routes with different HTTP
+//             Request handlers for NEF
+var CNTESTRoutes = []Route{
+	// Traffic Influence Routes
+	{
+		"ReadAllTrafficInfluenceSubscription",
+		strings.ToUpper("Get"),
+		"npcf-policyauthorization/v1/app-sessions/{appSessionId}",
+		PolicyAuthorizationAppSessionGet,
+	},
+	{
+		"CreateTrafficInfluenceSubscription",
+		strings.ToUpper("Post"),
+		"npcf-policyauthorization/v1/app-sessions",
+		PolicyAuthorizationAppSessionCreate,
+	},
+}
+
+// NewCnTestRouter : This function creates and initializes a CN-TEST Router with all
+//                the available routes for NEF Module. This router object is
+//                defined in "github.com/gorilla/mux" package.
+//  Input Args:
+//     - cnTestCtx: This is NEF Module Context. This contains the NEF Module Data.
+//  Output Args:
+//     - error: retruns pointer to created mux.Router object
+func NewCnTestRouter(cnTestCtx *cnTestContext) *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	for _, route := range CNTESTRoutes {
+
+		var handler http.Handler = route.Handler
+		handler = cnTestRouteLogger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+	}
+
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(
+				r.Context(),
+				cnTestCtxKey("cnTestCtx"),
+				cnTestCtx)
+
+			if nefCtx.cfg.OAuth2Support {
+				if cnTestValidateAccessToken(w, r) {
+					next.ServeHTTP(w, r.WithContext(ctx))
+				}
+			} else {
+				//OAuth2 disabled
+				next.ServeHTTP(w, r.WithContext(ctx))
+			}
+		})
+	})
+	return router
+}
+
+func cnTestValidateAccessToken(w http.ResponseWriter, r *http.Request) bool {
+
+	reqToken := r.Header.Get("Authorization")
+
+	if len(reqToken) == 0 {
+		log.Info("Authorization header missing")
+		//Authorization header is not present
+		w.Header().Set("WWW-Authenticate", "Bearer realm="+r.RequestURI)
+
+		w.WriteHeader(http.StatusUnauthorized)
+		return false
+	}
+
+	//Get the token
+	splitToken := strings.Split(reqToken, "Bearer ")
+	reqToken = splitToken[1]
+
+	status, err := oauth2.ValidateAccessToken(reqToken)
+
+	if err != nil {
+		log.Infoln("Token Validation failed")
+		if status == oauth2.StatusInvalidToken {
+			w.Header().Set("WWW-Authenticate", "Bearer realm="+r.RequestURI)
+			w.WriteHeader(http.StatusUnauthorized)
+			return false
+		} else if status == oauth2.StatusBadRequest {
+			w.WriteHeader(http.StatusBadRequest)
+			return false
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return false
+	}
+	return true
+}
+
+// cnTestRouteLogger : This function logs data received in HTTP request.
+// Input Args:
+//    - httpHandler: This is HTTP handler function pointer for HTTP Request
+//                   Received
+//    - name: This is route name.
+// Output Args:
+//    - httpHandler: This is HTTP handler function pointer for HTTP Request
+//                   Received. This HTTP Handler is actually the updated HTTP
+//                   Handler. The updated HTTP Handler now can logging of HTTP
+//                   Request Info also
+func cnTestRouteLogger(httpHandler http.Handler, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		log.Infof("HTTP Request Received :%s", r.Proto)
+		log.Infof("===============================================")
+		log.Infof(" Method : %s ", r.Method)
+		log.Infof(" URL PATH : %s", r.RequestURI)
+		log.Infof(" Route Name : %s", name)
+		log.Infof("===============================================")
+		log.Infof("HTTP Request Handling -- STARTS")
+
+		httpHandler.ServeHTTP(w, r)
+
+		log.Infof("HTTP Request Handling -- ENDS. Time Taken: %s",
+			time.Since(start))
+	})
+}
+
+
+func PolicyAuthorizationAppSessionGet(w http.ResponseWriter,
+	r *http.Request) {
+		log.Infoln("PolicyAuthorizationAppSessionGet -- Entered")
+}
+
+func PolicyAuthorizationAppSessionCreate(w http.ResponseWriter,
+	r *http.Request) {
+		log.Infoln("PolicyAuthorizationAppSessionCreate -- Entered")
+}
---
+++ ngc/pkg/cn-test/cn_test_server.go
@@ -0,0 +1,219 @@
+/* SPDX-License-Identifier: Apache-2.0
+* Copyright (c) 2019-2020 Intel Corporation
+ */
+
+package ngccntest
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	logtool "github.com/otcshare/common/log"
+	"golang.org/x/net/http2"
+)
+
+// CnTestApp structure to store the variables/contexts for access in UT
+type CnTestApp struct {
+	cnTestRouter *mux.Router
+	cnTestCtx    *cnTestContext
+}
+
+// NefAppG is the NEF App variable which can be used for accessing the
+// global contexts
+var CnTestApp CnTestApp
+
+// Log handler initialized. This is to be used throughout the CN-TEST module for
+// logging
+var log = logtool.DefaultLogger.WithField("CN-TEST", nil)
+
+//HTTPConfig contains the configuration for the HTTP 1.1
+type HTTPConfig struct {
+	Endpoint string `json:"endpoint"`
+}
+
+//HTTP2Config Contains the configuration for the HTTP2
+type HTTP2Config struct {
+	Endpoint      string `json:"endpoint"`
+	CNTESTServerCert string `json:"CnTestServerCert"`
+	CNTESTServerKey  string `json:"CnTestServerKey"`
+	AfClientCert  string `json:"AfClientCert"`
+}
+
+// Config contains NEF Module Configuration Data Structure
+type Config struct {
+	//MaxAFSupport              int    `json:"maxAFSupport"`
+	//UpfNotificationResURIPath string `json:"UpfNotificationResUriPath"`
+	HTTPConfig                HTTPConfig
+	HTTP2Config               HTTP2Config
+	//AfServiceIDs              []interface{} `json:"afServiceIDs"`
+	OAuth2Support             bool          `json:"OAuth2Support"`
+}
+
+// CN-TEST Module Context Data Structure
+type cnTestContext struct {
+	cfg Config
+	//cntest cnTestData
+}
+
+/* Go Routine is spawned here for starting HTTP Server */
+func startHTTPServer(server *http.Server,
+	stopServerCh chan bool) {
+	if server != nil {
+		log.Infof("HTTP 1.1 listening on %s", server.Addr)
+		if err := server.ListenAndServe(); err != nil {
+			log.Errf("HTTP server error: " + err.Error())
+		}
+	}
+	stopServerCh <- true
+}
+
+/* Go Routine is spawned here for starting HTTP-2 Server */
+func startHTTP2Server(serverHTTP2 *http.Server, cntestCtx *cnTestContext,
+	stopServerCh chan bool) {
+	if serverHTTP2 != nil {
+		log.Infof("HTTP 2.0 listening on %s", serverHTTP2.Addr)
+		if err := serverHTTP2.ListenAndServeTLS(
+			cnTestCtx.cfg.HTTP2Config.NefServerCert,
+			cnTestCtx.cfg.HTTP2Config.NefServerKey); err != nil {
+			log.Errf("HTTP2server error: " + err.Error())
+		}
+	}
+	stopServerCh <- true
+}
+
+// runServer : This function creates a Router object and starts a HTTP Server
+//             in a separate go routine. Also it listens for NEF module
+//             running context cancellation event in another go routine. If
+//             cancellation event occurs, it shutdowns the HTTP Server.
+// Input Args:
+//   - ctx:    NEF Module Running context
+//   - nefCtx: This is NEF Module Context. This contains the NEF Module Data.
+// Output Args:
+//    - error: retruns no error. It only logs the error if any happened while
+//             starting the HTTP Server
+func runServer(ctx context.Context, cnTestCtx *cnTestContext) error {
+
+	var err error
+	var server, serverHTTP2 *http.Server
+
+	/* CnTestRouter obeject is created. After creation this object contains all
+	 * the HTTP Service Handlers. These handlers will be called when HTTP
+	 * server receives any HTTP Request */
+	cnTestRouter := NewCnTestRouter(cnTestCtx)
+	CnTestAppG.CnTestRouter = cnTestRouter
+
+	// 1 for http2, 1 for http and 1 for the os signal
+	numchannels := 3
+
+	// Check if http and http 2 are both configured to determine number
+	// of channels
+
+	if nefCtx.cfg.HTTPConfig.Endpoint == "" {
+		log.Info("HTTP Server not configured")
+		numchannels--
+	} else {
+		// HTTP Server object is created
+		server = &http.Server{
+			Addr:           cnTestCtx.cfg.HTTPConfig.Endpoint,
+			Handler:        cnTestRouter,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+	}
+
+	if nefCtx.cfg.HTTP2Config.Endpoint == "" {
+		log.Info("HTTP 2 Server not configured")
+		numchannels--
+	} else {
+		serverHTTP2 = &http.Server{
+			Addr:           cnTestCtx.cfg.HTTP2Config.Endpoint,
+			Handler:        cnTestRouter,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+
+		if err = http2.ConfigureServer(serverHTTP2,
+			&http2.Server{}); err != nil {
+			log.Errf("failed at configuring HTTP2 server")
+			return err
+		}
+	}
+	if server == nil && serverHTTP2 == nil {
+		log.Err("HTTP Servers are not configured")
+		return errors.New("HTTP Endpoints config missing")
+	}
+
+	stopServerCh := make(chan bool, numchannels)
+
+	/* Go Routine is spawned here for listening for cancellation event on
+	 * context */
+	go func(stopServerCh chan bool) {
+		<-ctx.Done()
+		if server != nil {
+			log.Info("Executing graceful stop for HTTP Server")
+			if err = server.Close(); err != nil {
+				log.Errf("Could not close HTTP server: %#v", err)
+			}
+			log.Info("HTTP server stopped")
+		}
+
+		if serverHTTP2 != nil {
+
+			if err = serverHTTP2.Close(); err != nil {
+				log.Errf("Could not close HTTP2 server: %#v", err)
+			}
+			log.Info("HTTP2 server stopped")
+		}
+
+		stopServerCh <- true
+	}(stopServerCh)
+
+	/* Go Routine is spawned here for starting HTTP Server */
+	go startHTTPServer(server, stopServerCh)
+	/* Go Routine is spawned here for starting HTTP-2 Server */
+	go startHTTP2Server(serverHTTP2, nefCtx, stopServerCh)
+	/* This self go routine is waiting for the receive events from the spawned
+	 * go routines */
+	<-stopServerCh
+	<-stopServerCh
+	if numchannels == 3 {
+		<-stopServerCh
+	}
+	log.Info("Exiting CN-TEST server")
+	return nil
+
+}
+
+// Run : This function creates CN-TEST Server
+func Run(ctx context.Context, cfgPath string) error {
+
+//	err := loadJSONConfig(cfgPath, &cnTestCtx.cfg)
+	if err != nil {
+		log.Errf("Failed to load NEF configuration: %v", err)
+		return err
+
+	}
+//	printConfig(cnTestCtx.cfg)
+
+	return runServer(ctx, &cnTestCtx)
+}
+
+func printConfig(cfg Config) {
+
+	log.Infoln("********************* NGC CN-TEST CONFIGURATION ******************")
+	//log.Infoln("APIRoot: ", cfg.CnTestAPIRoot)
+	log.Infoln("OAuth2Support:", cfg.OAuth2Support)
+	log.Infoln("-------------------------- CN-TEST SERVER ----------------------")
+	log.Infoln("EndPoint(HTTP): ", cfg.HTTPConfig.Endpoint)
+	log.Infoln("EndPoint(HTTP2): ", cfg.HTTP2Config.Endpoint)
+	log.Infoln("ServerCert(HTTP2): ", cfg.HTTP2Config.NefServerCert)
+	log.Infoln("ServerKey(HTTP2): ", cfg.HTTP2Config.NefServerKey)
+	log.Infoln("AFClientCert(HTTP2): ", cfg.HTTP2Config.AfClientCert)
+	log.Infoln("*************************************************************")
+
+}
